c7fa6eaa1697f123299001ea267804b8644fae2b
Mina Mekhael <mina.mekhael@ymail.com>
2019-06-12 12:27:17 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
 
+  resources :tips, only: [:index, :create]
   get '/auth', to: 'sessions#create'
   get '/friends', to: 'searches#friends'
 
